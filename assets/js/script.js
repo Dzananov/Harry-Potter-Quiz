09b7c1feef65728cb3a7684f013f38cd50af9c4c
Dzananov <danni_daa@hotmail.com>
2022-08-07 19:09:01 +0000

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-console.log("working")
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById ('answers')
+const submitButton =document.getElementById ('Submitbutton')
+
+
+function setQuestion() {
+   submitButton.classList.add('hide')
+   nextQuestion()
+}
+
+function nextQuestion(question){
+questionElement.innerText = question.question
+}
+
+function score(){
+
+}
+
+
+
+
+
+
+
+
+const question = [
+    {
+        question: 'Who is Harrys best friends',
+        answers: [
+            {text: 'Ron and Hermoine', correct: true},
+            {text: 'Malfoe and Neville', correct: false}
+        ]
+    }
+    
+]
+
+
