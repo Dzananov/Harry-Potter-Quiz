de7b1c6e7bd36a8d463e5992e7eafd64b19f0534
Dzananov <danni_daa@hotmail.com>
2022-08-11 08:36:24 +0000

    js questions done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,28 @@
 const startButton = document.getElementById('start')
-const submitButton = document.getElementById('submit')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer')
-const a = document.getElementById('a')
-const b = document.getElementById('b')
+const submitButton = document.getElementById ('submit')
+var rightAnswer = document.getElementById('answera','answerd' )
 
 
-
-
-startButton.addEventListener('click', showQuestion)
-
-
-function showQuestion(question) {
-
-questionElement.innerText = question.question
-    a.innerText = question.a
-    b.innerText = question.b
-}
-
-function selectAnswers() {
-      answerElement.forEach(answerElement => answerElement.checked = false)
-  }
-
+startButton.addEventListener('click', startGame)
  
 
-  const question = [
-    {
-        question: "Who is Harrys best friends",
-        a: "Hermonie and Ron",
-        b: "Malfoy and Neville",
-        
-        correct: "a",
-    },
-    {
-        question: "Who is the principal of Hogwarts",
-        a: "Snape",
-        b: "Dumbledore",
-        
-        correct: "b",
-    },
- ];
+
+function startGame() {
+  var question1 = document.getElementById ('question1'). innerHTML = "Who is Harrys best friends"
+  var answer1 = document.getElementById ('answera'). innerHTML = 'Hermonie And Ron'
+  var answer2 = document.getElementById ('answerb'). innerHTML = 'Malfoy and Neville'
+  rightAnswer.addEventListener('click', changeColor)
+  var question2 = document.getElementById ('question2'). innerHTML = "Who is the principal of Hogwarts"
+  var answer3 = document.getElementById ('answerc'). innerHTML = 'Snape'
+  var answer4 = document.getElementById ('answerd'). innerHTML = 'Dumbledor'
+
+  var question3 = document.getElementById ('question3'). innerHTML = "What is Harrys Owl called"
+  var answer5 = document.getElementById ('answere'). innerHTML = 'Hedwig'
+  var answer6 = document.getElementById ('answerf'). innerHTML = 'Scabbers'
 
 
-  
+}
+
+function changeColor() {
+  rightAnswer.style.backgroundColor = 'lightgreen';
+}
