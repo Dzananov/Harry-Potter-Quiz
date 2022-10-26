c9e09867744fceebfa4db54ab39d6a7307da535e
Dzananov <danni_daa@hotmail.com>
2022-10-26 07:22:45 +0000

    add start function, did last changes in css and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,49 @@
 const QUESTION = [{"question": "Who is Harrys best friends",  "answer1" : "Hermonie and Ron","answer2": "Malfoy and Neville", "correct": "Hermonie and Ron"}, 
 {"question": "Who is ther principal of Hogwarts?",  "answer1" : "Snape", "answer2": "Dumbeldore", "correct":"Dumbeldore"},
 {"question": "What is Harrys owl called?",  "answer1" : "Hedwig", "answer2": "Dolores", "correct":"Hedwig"},
-{"question": "Wich proffessor can turn into a cat?",  "answer1" : "Nymphadora Tonks", "answer2": "Minerva McGonagall", "correct":"Minerva McGonagall"},]
+{"question": "Wich proffessor can turn into a cat?",  "answer1" : "Nymphadora Tonks", "answer2": "Minerva McGonagall", "correct":"Minerva McGonagall"},
+{"question": "What kind of creature can Remus Lupin turn into?",  "answer1" : "Dementore", "answer2": "Werewolf", "correct":"Werewolfl"},
+{"question": "What kind of monster did attack the students and other in the seconde book?",  "answer1" : "A Fenix bird", "answer2": "Snake", "correct":"Snake"},
+{"question": "Aragon is Hagrids friend but what kind of creature is he?",  "answer1" : "Spider", "answer2": "Dog", "correct":"Spider"},
+{"question": "Who becomes Harrys girlfriend in the 6th book?",  "answer1" : "Ginny", "answer2": "Kathy", "correct":"Ginny"},
+{"question": "Who is Sirius Black?",  "answer1" : "Harrys godfather", "answer2": "a runnaway convict", "correct":"Harrys godfather"},
+{"question": "Wich positions does Harry have in quiddich?",  "answer1" : "Chaser", "answer2": "Seeker", "correct":"Seeker"},
+{"question": "Wich spell can make things fly?",  "answer1" : "avada kadavra", "answer2": "Wingardium leviosa", "correct":"Wingardium leviosa"},]
 
 /*Questioncount*/
 let currentQuestion = 0;
 let correctQuestion = 0;
 
-let reset = document.getElementById("reset")
-
-
-
-
-/*counts correctquestions*
-*got element for starbutton,answersbuttons and submitbutton*/
+/*Defining answer buttons and questionsarea aswell as start and reset buttons*/
 
 
 let answer1 = document.getElementById("answer1");
 let answer2 = document.getElementById("answer2");
 let submit = document.getElementById("submit");
 let questionarea = document.getElementById('question-area')
+let reset = document.getElementById("reset")
+let start = document.getElementById("start")
 
-/*calling events when clicked on*
-*The first one calls the displayquestionfucntion etc...*/
+/*calling events when clicked on*/
 
 answer1.addEventListener('click', selectedAnswer);
 answer2.addEventListener('click', selectedAnswer);
 reset.addEventListener('click', restart)
+start.addEventListener('click', displayNextQuestion)
 
-
-/*Here are the questions shown*/
+startGame()
+/*The first function calls the startbutton when user clicks on the startbutton questionarea will be shown and
+game will start*/
+function startGame(){
+  document.getElementById("start").classList.remove('hide')
+  document.getElementById("question-area").classList.add('hide')
+}
+/* In this function question and answers will be shown and the user can click on wichever answer he/she think is correct. ext
+question will be shown directly after*/
 function displayNextQuestion() {
   let q = QUESTION[currentQuestion];
   document.getElementById("question-area").classList.remove('hide')
+  document.getElementById("start").classList.add('hide')
    document.getElementById("question").innerText = q.question;
    document.getElementById("answer1").innerText = q.answer1;
    document.getElementById("answer2").innerText = q.answer2;
@@ -41,22 +52,17 @@ function displayNextQuestion() {
    
   } 
 
-/*submitbutton apears when user choose an answer*/
+/*This fuction manages the correction of user answers and also scorecount. When all the questions are answerd user will be redirected to next function*/
 function selectedAnswer(Event) {
   
   selectedAnswer = Event.currentTarget.innerText
-   /*This code for my scoreboard is from the Love-math project
- For every right answer the scorepoint rises*/
   let q = QUESTION[currentQuestion];
   if (selectedAnswer === q.correct) 
-    
-  
-   { let oldScore = parseInt(document.getElementById("score").innerText);
+    { let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
     correctQuestion ++;
-  }
+    }
   currentQuestion++;
-  
   if (currentQuestion < QUESTION.length ) {
     displayNextQuestion()
     } else {
@@ -65,7 +71,7 @@ function selectedAnswer(Event) {
       
     }
   }
-
+/* When all the questions been answerd user can choose to restart the game*/
   function restart() {
 
     currentQuestion = 0;
@@ -74,16 +80,6 @@ function selectedAnswer(Event) {
     document.getElementById("resultSection").classList.add('hide')
     document.getElementById("question-area").classList.remove("hide")
     displayNextQuestion();
-    
-    
   }
-
-
-  // function reset (){
-
-  
-//     alert("you've got " + correctQuestion)
-//     }
-// }
-
+startGame()
 displayNextQuestion()
